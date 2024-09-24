8b84c2fa288d8d976e2ef159d4b853a4ba4af612
Denys <duiugfihin@gmail.com>
2024-09-24 20:31:59 +0300

    Add project files.

---
+++ CourseProjectPlanner/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+using CourseProjectPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CourseProjectPlanner.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+        public IActionResult Spends()
+        {
+            return View();
+        }
+
+        public IActionResult Statistics(int months)
+        {
+            switch (months)
+            {
+                case 1:
+                    { ViewBag.Months = $"останній {months} місяць"; break; }
+
+                case 2:
+                case 3:
+                case 4:
+                    { ViewBag.Months = $"останні {months} місяці"; break; }
+
+                default:
+                    { ViewBag.Months = $"останні {months} місяців"; break; }
+
+            }
+
+            return View();
+        }
+
+		
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ CourseProjectPlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourseProjectPlanner.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
