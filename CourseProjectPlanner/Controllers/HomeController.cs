59356ab863d7ee407e03f25f15daa528ec82fc38
Denys <duiugfihin@gmail.com>
2024-10-02 21:04:32 +0300

    Додано форми для реєстрації та входу в обліковий запис з відповідними посиланнями

--- CourseProjectPlanner/Controllers/HomeController.cs
+++ CourseProjectPlanner/Controllers/HomeController.cs
@@ -48,9 +48,18 @@ namespace CourseProjectPlanner.Controllers
             return View();
         }
 
-		
+		public IActionResult Registration()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
