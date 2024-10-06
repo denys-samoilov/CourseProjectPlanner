1ca4c7f146c10283fd6bcbb896b705fb10a5887d
Denys <duiugfihin@gmail.com>
2024-10-06 19:25:31 +0300

    Підключено БД

---
+++ CourseProjectPlanner/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using CourseProjectPlanner.Models;
+global using CourseProjectPlanner.Data;
+global using Microsoft.EntityFrameworkCore;
+
