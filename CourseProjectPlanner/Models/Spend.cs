7ef9a54ff1a7eff21213dc82ae0b7f7f8d52d23c
Denys <133922338+denys-samoilov@users.noreply.github.com>
2024-11-07 22:10:34 +0200

    Комміт перед міграцією

--- CourseProjectPlanner/Models/Spend.cs
+++ CourseProjectPlanner/Models/Spend.cs
@@ -4,6 +4,7 @@
     {
         [Key]
         public int SpendId { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
         public decimal Price { get; set; }
         public string? Description { get; set; }
 
